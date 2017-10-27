723a8fd167a0646b2bef858e7b4659055a9adcf4
Jan Librowski <janlibrowski@gmail.com>
2017-10-27 19:22:51 +0200

    Add message relations

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -3,4 +3,5 @@
 class Administrator < ApplicationRecord
   include Trestle::Auth::ModelMethods
   include Trestle::Auth::ModelMethods::Rememberable
+  has_many :messages, dependent: :restrict_with_error
 end
