c08a90f04553eeaa030730ef4b601a3a06c9f991
Jan Librowski <janlibrowski@gmail.com>
2017-10-27 20:40:50 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+FactoryBot.create_list(:administrator, 3)
+FactoryBot.create_list(:donor, 15)
+FactoryBot.create_list(:message, 30, administrator_id: rand(3) + 1, donor_id: rand(15) + 1)
