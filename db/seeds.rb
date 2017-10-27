afb31be80201d54960a5e718f14db546c2b44add
Jan Librowski <janlibrowski@gmail.com>
2017-10-28 01:27:49 +0200

    Add material layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.create_list(:donor, 15)
+FactoryBot.create_list(:admin, 3)
 30.times do
   FactoryBot.create(:message, donor: Donor.all.sample)
 end
--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,5 +2,7 @@
 
 FactoryBot.define do
   factory :admin do
+    sequence(:email) { |n| "admin#{n}@example.com" }
+    password "password"
   end
 end
