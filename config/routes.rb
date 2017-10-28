b4849ae4756aebd8f407384b62d8764a3038f6ca
Jan Librowski <janlibrowski@gmail.com>
2017-10-28 02:48:12 +0200

    Add message new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :donors, only: %i[show index]
+  resources :messages, only: %i[new create show]
 
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
